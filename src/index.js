b815233168c2e458927a0bc0b7a3da487a12ae76
luke01455 <luke0145555@hotmail.co.uk>
2019-12-15 22:33:11 +0000

    set up a simple count subscription

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
-import { GraphQLServer } from 'graphql-yoga';
+import { GraphQLServer, PubSub } from 'graphql-yoga';
 import db from './db'
 import Query from './resolvers/Query'
-import User from './resolvers/User'
+import Subscription from './resolvers/Subscription'
 import Mutation from './resolvers/Mutation'
 import Post from './resolvers/Post'
 import Comment from './resolvers/Comment'
+import User from './resolvers/User'
+
+const pubsub = new PubSub()
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
     resolvers: {
         Query,
         Mutation,
+        Subscription,
         User, 
         Post,
         Comment
@@ -18,6 +22,7 @@ const server = new GraphQLServer({
     // gives all files on server the db file as context
     context: {
         db,
+        pubsub
     }
 })
 
