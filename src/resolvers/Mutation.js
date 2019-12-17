c9034cddadc7188eaba7b4a61bbef92647a12f5b
luke01455 <luke0145555@hotmail.co.uk>
2019-12-17 17:03:28 +0000

    created enum for mutationtype

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -175,22 +175,34 @@ const Mutation = {
         }
 
         db.comments.push(comment)
-        pubsub.publish(`comment ${args.data.post}`, { comment })
+        pubsub.publish(`comment ${args.data.post}`, { 
+            comment: {
+                mutation: 'CREATED',
+                data: comment
+            }
+         })
 
         return comment
 
     },
-    deleteComment(parent, args, { db }, info) {
+    deleteComment(parent, args, { db, pubsub }, info) {
         const commentIndex = db.comments.findIndex((comment) => comment.id === args.id)
 
         if(commentIndex === -1) {
             throw new Error('comment not found')
         }
-        const deletedComments = db.comments.splice(commentIndex, 1)
+        const [deletedComment] = db.comments.splice(commentIndex, 1)
 
-        return deletedComments[0]
+        pubsub.publish(`comment ${deletedComment.post}`, { 
+            comment: {
+                mutation: 'DELETED',
+                data: deletedComment
+            }
+         })
+
+        return deletedComment
     },
-    updateComment(parent, args, { db }, info) {
+    updateComment(parent, args, { db, pubsub }, info) {
         const {data, id} = args
         const comment = db.comments.find((comment) => comment.id === id)
 
@@ -201,7 +213,14 @@ const Mutation = {
         if(typeof data.text === 'string') {
             comment.text = data.text
         }
-        
+
+        pubsub.publish(`comment ${comment.post}`, { 
+            comment: {
+                mutation: 'UPDATED',
+                data: comment
+            }
+         })
+
         return comment
     }
 }
